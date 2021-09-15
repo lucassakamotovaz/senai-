baef493ee535be7bba3dde75a77fc379378ef2ee
Lucas sakamoto vaz <lucassakamotovaz2@hotmail.com>
2021-09-14 21:05:05 -0300

    Remota2

---
+++ Remota2.js
@@ -0,0 +1,16 @@
+var numeroAluno = 14
+var contador
+for(contador=0;contador<=numeroAluno;contador++){
+    if(contador%2 == 0){
+        
+        if(contador== 0){
+            console.log("esse numero é zero")
+        } else{
+            console.log("esse numero é par "+contador)
+        }
+    } else{
+        console.log("esse numero é impar " +contador)
+    }
+}
+    
+       
